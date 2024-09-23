9081ac7463a2e84446f0b8382aaa76e17bf186fc
Nosrey <nosrey135@gmail.com>
2024-09-23 14:51:04 -0400

    variable language agregadao

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 // importo Homepage
 import { Homepage } from './UIElements/Homepage/Homepage';
 
+const language = 'spanish';
+
 function App() {
   return (
     <div className="App">
