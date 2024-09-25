a6caa3cf9cd03c6fd136ffcd774e0bac191a77e5
Ansony Rojas <ansonyrojas2015@gmail.com>
2024-09-25 14:42:59 -0400

    Only Block Fourth

--- src/UIElements/Homepage/Homepage.js
+++ src/UIElements/Homepage/Homepage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import BlockTwo from '../BlockTwo/BlockTwo'
 import styles from './styles/home.module.css'
 import BlockFourth from '../BlockFourth/BlockFourth'
 
@@ -7,7 +6,6 @@ export const Homepage = () => {
   return (
     <div className={styles.fatherContainer}>Homepage
       <BlockFourth />
-      <BlockTwo />
     </div>
   )
 }
