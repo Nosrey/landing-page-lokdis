3633e1b775f0b5b3db5137fd0e2ebb8bc876ac55
Ansony Rojas <ansonyrojas2015@gmail.com>
2024-09-30 16:18:48 -0400

    Carrusel completado v1

---
+++ src/UIElements/Carrousel/Carrousel.jsx
@@ -0,0 +1,31 @@
+import styles from './styles/carrousel.module.css';
+import comeIn from '../Assets/come-in-carrousel.png';
+
+const Carrousel = () => {
+    return (
+        <div className={styles.fatherContainer}>
+            <div className={styles.banner}>
+                <div className={styles.textContainer}>
+                    <h2 className={styles.textCarrousel}>
+                        ¡Súmate a la comunidad ahora!
+                    </h2>
+                    <img src={comeIn} alt="logo" />
+                    <h2 className={styles.textCarrouselTwo}>
+                        ¡Súmate a la comunidad ahora!
+                    </h2>
+                    <img src={comeIn} alt="logo" />
+                    <h2 className={styles.textCarrousel}>
+                        ¡Súmate a la comunidad ahora!
+                    </h2>
+                    <img src={comeIn} alt="logo" />
+                    <h2 className={styles.textCarrouselTwo}>
+                        ¡Súmate a la comunidad ahora!
+                    </h2>
+                    <img src={comeIn} alt="logo" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Carrousel;
--- src/UIElements/Homepage/Homepage.js
+++ src/UIElements/Homepage/Homepage.js
@@ -4,10 +4,12 @@ import BlockFive from '../BlockFive/BlockFive'
 import BlockFourth from '../BlockFourth/BlockFourth'
 import BlockTwo from '../BlockTwo/BlockTwo';
 import { Bloque1 } from '../Bloque-1/Bloque1'
+import Carrousel from '../Carrousel/Carrousel';
 
 export const Homepage = ({ language, setLanguage }) => {
   return (
     <div className={styles.fatherContainer}>Homepage
+      <Carrousel />
       <BlockFive language={language} />
     </div>
   )
