a0a35114ac176d6152c891901e17c6591aab6272
Ansony Rojas <ansonyrojas2015@gmail.com>
2024-10-30 14:17:59 -0400

    Landing para la presentacion de hoy 2:30pm

--- src/UIElements/Bloque-1/Bloque1.js
+++ src/UIElements/Bloque-1/Bloque1.js
@@ -5,11 +5,13 @@ import ReactPlayer from 'react-player'
 import arrow from './Images/arrow.gif';
 // import phoneSquare from './Images/phoneSquare.png';
 import { useEffect, useRef, useState } from "react";
+import { FaVolumeMute, FaVolumeUp } from 'react-icons/fa';
 // import video from '../Assets/video.mp4';
 
 export const Bloque1 = ({ language, setLanguage, numberOfPerson }) => {
   const isSpanish = language === "spanish";
   const [loading, setLoading] = useState(true);
+  const [isMuted, setIsMuted] = useState(true);
 
   const containerRef = useRef(null);
 
@@ -20,6 +22,7 @@ export const Bloque1 = ({ language, setLanguage, numberOfPerson }) => {
       window.open('https://b6j96f1eelu.typeform.com/to/mlppGgt6', '_blank');
     }
   }
+
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -49,15 +52,10 @@ export const Bloque1 = ({ language, setLanguage, numberOfPerson }) => {
   }, []);
 
 
+
   return (
     <div className="fondo">
 
-      {/* <button
-        onClick={() => setLanguage(isSpanish ? "english" : "spanish")}
-        className="botonIdioma"
-      >
-        {isSpanish ? "cambiar" : "change"}
-      </button> */}
       <div className="columnas">
         <div className="columnaSingular leftBloq1">
 
@@ -79,25 +77,36 @@ export const Bloque1 = ({ language, setLanguage, numberOfPerson }) => {
                 {isSpanish ? "Descubre LokDis, una app para ver el mundo real en la que podrás solicitar momentos y " : "Discover LokDis, an app to view the real world in which you can request unfiltered moments and "}
                 <span>{isSpanish ? "vivir experiencias auténticas." : "live authentic experiences."}</span>
               </p>
-
-              <div className='phoneView2Bloq1' style={{ margin: '25px 0' }}>
-                {loading && <div className="spinnerBloq1"></div>}
-                <div className="phone-frame" style={{ margin: '0', display: loading ? 'none' : 'block' }}>
-                  <div className="phone-content">
-                    {/* url='https://vimeo.com/1023344517' */}
-                    <ReactPlayer
-                      url='https://lokdis.com/multimedia/Video_landing_LokDis.mp4'
-                      controls
-                      playing={true}
-                      loop={true}
-                      muted={true}
-                      onReady={() => setLoading(false)}
-                      width='100%'
-                      height='100%'
-                    />
+              {window.innerWidth <= 500 &&
+                <div className='phoneView2Bloq1' style={{ margin: '25px 0' }}>
+                  {loading && <div className="spinnerBloq1"></div>}
+                  <div className="phone-frame" style={{ margin: '0', display: loading ? 'none' : 'block' }}>
+                    <div className="phone-content">
+                      <ReactPlayer
+                        url='https://vimeo.com/1023344517'
+                        controls={false}
+                        playing={true}
+                        loop={true}
+                        volume={isMuted ? 0 : 1}
+                        onReady={() => setLoading(false)}
+                        width='100%'
+                        height='100%'
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
+
+
+
+              }
+              <button
+                style={{ margin: '0px auto 10px auto', display: (!loading && window.innerWidth <= 500) ? 'block' : 'none' }}
+                onClick={() => setIsMuted(!isMuted)}
+                className={`botonPhone mute-switch ${isMuted ? 'muted' : 'unmuted'}`}
+              >
+                {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
+              </button>
+
 
             </div>
 
@@ -109,34 +118,47 @@ export const Bloque1 = ({ language, setLanguage, numberOfPerson }) => {
         </div>
 
         {/* lado derecho */}
-        <div className="columnaSingular rightBloq1" style={{ width: "40%" }} ref={containerRef}>
+        <div className="columnaSingular rightBloq1" style={{ width: "40%", position: 'relative' }} ref={containerRef}>
           {/* <img
             src={phoneView}
             alt="phoneView"
             className="phoneView"
           /> */}
+
+
           <div className='phoneDeskBloq1'>
             {loading && <div className="spinnerBloq1"></div>}
-            <div className="phone-frame" style={{ display: loading ? 'none' : 'block' }}>
-              <ReactPlayer
-                url='https://lokdis.com/multimedia/Video_landing_LokDis.mp4'
-                controls
-                playing={true}
-                loop={true}
-                muted={true}
-                onReady={() => setLoading(false)}
-                width='100%'
-                height='100%'
-              />
-            </div>
+            {window.innerWidth > 500 &&
+
+              <div className="phone-frame" style={{ display: loading ? 'none' : 'block' }}>
+                <ReactPlayer
+                  url='https://vimeo.com/1023344517'
+                  controls={false}
+                  playing={true}
+                  loop={true}
+                  volume={isMuted ? 0 : 1}
+                  onReady={() => setLoading(false)}
+                  width='100%'
+                  height='100%'
+                />
+              </div>
+            }
           </div>
+
+          <button
+            style={{ display: (!loading && window.innerWidth > 500) ? 'block' : 'none' }}
+            onClick={() => setIsMuted(!isMuted)}
+            className={`botonDesk mute-switch ${isMuted ? 'muted' : 'unmuted'}`}
+          >
+            {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
+          </button>
 
           {/* <video
             autoPlay
             playsInline
             className="phoneView"
             src={video}
-            >              
+            >
             </video>           */}
         </div>
       </div>
